aabc4d48911689af1d9ad422a188911efffb3950
karmacoma <karma@coma.lol>
2022-08-26 15:55:17 -0700

    Include deploy script

--- script/SnowProgram.s.sol
+++ script/SnowProgram.s.sol
@@ -3,10 +3,13 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
 
-contract CounterScript is Script {
+import {SnowProgram} from "src/SnowProgram.sol";
+
+contract SnowProgramDeploy is Script {
     function setUp() public {}
 
     function run() public {
         vm.broadcast();
+        new SnowProgram();
     }
 }
