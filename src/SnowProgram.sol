0f809be2548033966a64fa2cb02e25c61fea9bc0
karmacoma <karma@coma.lol>
2022-08-27 10:53:03 -0700

    Add examples

--- src/SnowProgram.sol
+++ src/SnowProgram.sol
@@ -8,6 +8,11 @@ contract SnowProgram is ISnowV1Program {
         return "SnowProgram";
     }
 
+    /// @dev value is interpreted as a 16*16 canvas of black and white pixels, examples:
+    /// @dev    0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff is an all white square
+    /// @dev    0x0000000000000000000000000000000000000000000000000000000000000000 is an all black square (will be rejected by SnowV1.storeProgram() though)
+    /// @dev    ~canvas[lastUpdatedIndex] inverts the colors of the last updated square
+    /// @dev sprites can be hardcoded as uint256 and generated at https://snow.computer/operators
     function run(uint256[64] memory canvas, uint8 lastUpdatedIndex) external returns (uint8 index, uint256 value) {
         index = 42;
         value = 42;
